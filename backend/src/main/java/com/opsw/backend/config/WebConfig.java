24b507b0cdc6c0f468407648267396e1ffedcad6
송성민 <songcho0000@naver.com>
2025-11-27 00:21:15 +0900

    feat: add Webconfig

--- backend/src/main/java/com/opsw/backend/config/WebConfig.java
+++ backend/src/main/java/com/opsw/backend/config/WebConfig.java
@@ -6,10 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+        registry.addMapping("/**") // 모든 API 허용
+                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedMethods("*") // GET, POST, PUT, DELETE 등 모든 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(false); // Credential 필요 없으면 false
     }
 }
