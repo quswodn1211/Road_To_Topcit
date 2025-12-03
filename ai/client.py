f7d00002a8011a95cfb9ce9a67de6b82c9695eca
quswodn1211 <quswodn1211@gmail.com>
2025-12-03 20:20:08 +0000

    클라이언트 파일 변경

--- ai/client.py
+++ ai/client.py
@@ -1,12 +1,16 @@
 import asyncio
 import websockets
 
-EC2_IP = "1njwm13rrg08je-20.proxy.runpod.net"
-EC2_WS = f"ws://{EC2_IP}:8000/ws"  
+URL = "wss://a2u2mglnolktsnolktsn.proxy.runpod.net/ws"
+API_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhc3NzYSIsIm5hbWUiOiJqYWV3b28iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyfQ.Zm8kUhVG6a47XHN7YX1RQDTGA4dV4a7zuqOK48eCzEY"  
 
 
 async def main():
-    async with websockets.connect(f"ws://{EC2_IP}:8000") as ws:
+    async with websockets.connect(
+        URL,
+        additional_headers={"Authorization": f"Bearer {API_TOKEN}"}
+    ) as ws:
+        print("connect")
         await ws.send("소프트웨어 공학에 대해 알려줘")
         reply = await ws.recv()
         print("Reply:", reply)
